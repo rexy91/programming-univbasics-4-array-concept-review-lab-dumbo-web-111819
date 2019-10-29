ea6f5aa23e034d1708bba504333f62a1752f4cb9
Yu Hao Ye <rxye1991@gmail.com>
2019-10-29 00:32:58 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Using Loops and Arrays to Find Data
+gm# Using Loops and Arrays to Find Data
 
 ## Learning Goals
 
--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+	i = 0
+	result = 0
+	for i in i ... array.size()
+		if array[i] == value_to_find
+			result = i
+			return result
+		end
+	end
+	return nil
 end
 
 def find_max_value(array)
